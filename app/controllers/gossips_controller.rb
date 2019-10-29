46caeb31c2d345dc74f133e6d0bf19a88f0d7407
ladislasfontaine <fontaine.ladislas@gmail.com>
2019-10-29 17:28:36 +0100

    Main commit for day 2. Create a gossip.

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,44 @@
+class GossipsController < ApplicationController
+  def index
+    # Méthode qui récupère tous les potins et les envoie à la view index (index.html.erb) pour affichage
+    @gossips = Gossip.all
+    @users = User.all
+  end
+
+  def show
+    # Méthode qui récupère le potin concerné et l'envoie à la view show (show.html.erb) pour affichage
+    @gossip = Gossip.find(params[:id])
+    @gossip_creation_date = @gossip.created_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
+
+  def new
+    # Méthode qui crée un potin vide et l'envoie à une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
+  end
+
+  def create
+    @gossip = Gossip.new(title: params[:title], content: params[:content], user: User.first) #, user: User.first
+
+    if @gossip.save
+      flash[:success] = "You created a Gossip!"
+      redirect_to root_path
+    else
+      flash[:danger] = "A problem occured my friend."
+      render new_gossip_path
+    end
+  end
+
+  def edit
+    # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
+  end
+
+  def update
+    # Méthode qui met à jour le potin à partir du contenu du formulaire de edit.html.erb, soumis par l'utilisateur
+    # pour info, le contenu de ce formulaire sera accessible dans le hash params
+    # Une fois la modification faite, on redirige généralement vers la méthode show (pour afficher le potin modifié)
+  end
+
+  def destroy
+    # Méthode qui récupère le potin concerné et le détruit en base
+    # Une fois la suppression faite, on redirige généralement vers la méthode index (pour afficher la liste à jour)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  # resources :gossips, except: [:destroy] 
+  resources :gossips, except: [:destroy]
 
-  get '/gossips/:id', to: 'gossip#show', as: 'gossip'
+  # get '/gossips/:id', to: 'gossip#show', as: 'gossip'
   get '/author/:author_id', to: 'user#show', as: 'author'
   get '/team', to: 'team#index'
   get '/contact', to: 'contact#index'
   get '/welcome/:first_name', to: 'welcome#index'
 
-  root 'gossip#index'
+  root 'gossips#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
